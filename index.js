a77ebea47864fc4966000213f87747b2809e8210
Valeria Mora <valemora2018@gmail.com>
2023-11-24 15:33:59 -0600

    Parte 1/3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Ejercicio 1
+// Ejercicio 1 --------------------------------------------------------->
+
+const numeros = [];
+
+for (let i = 0; i < 10; i++){
+    const numeroAzar = Math.floor(Math.random()* 100); 
+    numeros.push(numeroAzar);
+}
+
+console.log(numeros);
+
+// Ejercicio 2 --------------------------------------------------------->
+
+
+
+// Ejercicio 3 --------------------------------------------------------->
+
